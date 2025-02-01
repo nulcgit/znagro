2c7c2dc995e67344eabefbf6d78972400f2b2af2
nulc <nulc@ya.ru>
2025-02-02 01:51:11 +0700

    Add lock file

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+)
 
 func main() {
-    fmt.Println("Znagro - knowledge aggregator for the Myaoogle system")
+	exePath, err := os.Executable()
+	if err != nil {
+		fmt.Println("Failed to get executable path:", err)
+		return
+	}
+	programDir := filepath.Dir(exePath)
+
+	lockFile := filepath.Join(programDir, "znagro.lock")
+
+	file, err := os.OpenFile(lockFile, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println("Failed to create lock file:", err)
+		return
+	}
+	defer file.Close()
+
+	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	if err != nil {
+		fmt.Println("Another instance of znagro is already running.")
+		return
+	}
+	defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
+
+	fmt.Println("Znagro - knowledge aggregator for the Myaoogle system")
+
+	defer os.Remove(lockFile)
 }
